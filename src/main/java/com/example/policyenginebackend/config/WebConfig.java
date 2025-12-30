f27874c5a23435728ac224fcb1d1365a98f7fb9b
kamaleshkannaa <120234666+kamaleshkannaa@users.noreply.github.com>
2025-12-30 15:46:02 +0530

    Fix delete rule condition using ruleId and conditionId add

--- src/main/java/com/example/policyenginebackend/config/WebConfig.java
+++ src/main/java/com/example/policyenginebackend/config/WebConfig.java
@@ -1,16 +1,20 @@
-//package com.example.policyenginebackend.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.*;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/api/**")
-//                .allowedOrigins("http://localhost:5173")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*");
-//    }
-//}
+package com.example.policyenginebackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://enterprise-policy-rule-engine.vercel.app"  // ← ADD THIS
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
+
