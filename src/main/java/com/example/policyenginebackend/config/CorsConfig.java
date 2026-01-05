81b017493c436295e27e3497fdfe36cabf25653d
kamaleshkannaa <120234666+kamaleshkannaa@users.noreply.github.com>
2026-01-05 14:46:49 +0530

    Fix CORS using WebMvcConfigurer

--- src/main/java/com/example/policyenginebackend/config/CorsConfig.java
+++ src/main/java/com/example/policyenginebackend/config/CorsConfig.java
@@ -1,42 +1,42 @@
-package com.example.policyenginebackend.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
-import java.util.List;
-
-@Configuration
-public class CorsConfig {
-
-    @Value("${CORS_ALLOWED_ORIGINS}")
-    private String allowedOrigins;
-
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(
-                Arrays.asList(allowedOrigins.split(","))
-        );
-
-        config.setAllowedMethods(List.of(
-                "GET", "POST", "PUT", "DELETE", "OPTIONS"
-        ));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
-    }
-}
-
-
-
+//package com.example.policyenginebackend.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Value("${CORS_ALLOWED_ORIGINS}")
+//    private String allowedOrigins;
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//        config.setAllowedOrigins(
+//                Arrays.asList(allowedOrigins.split(","))
+//        );
+//
+//        config.setAllowedMethods(List.of(
+//                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+//        ));
+//        config.setAllowedHeaders(List.of("*"));
+//        config.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source =
+//                new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsFilter(source);
+//    }
+//}
+//
+//
+//
