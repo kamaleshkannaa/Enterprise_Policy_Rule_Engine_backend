b4aec8589d2b83b4df4d40746ce6fb742f2f0aa0
kamaleshkannaa <120234666+kamaleshkannaa@users.noreply.github.com>
2026-01-05 14:48:33 +0530

    Fix CORS using WebMvcConfigurerr

--- src/main/java/com/example/policyenginebackend/config/WebConfig.java
+++ src/main/java/com/example/policyenginebackend/config/WebConfig.java
@@ -1,18 +1,18 @@
-package com.example.policyenginebackend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("https://enterprise-policy-rule-engine.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//package com.example.policyenginebackend.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**")
+//                .allowedOrigins("https://enterprise-policy-rule-engine.vercel.app")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
